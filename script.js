fcb04f4c5071211972d7d3437d5ca5b3243ae22d
Pinar Gulum <pinargulum@hotmail.com>
2024-12-24 23:37:59 -0500

    update

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-//Handle Enter key press for sending messages
+
 
 const messageInput = document.querySelector(".message-input");
-
+// create message element with dynamic classes and return it
+const createMessageElement = (content, classes) => {
+  const div = document.createElement("div");
+  div.classList.add("message", classes);
+  div.innerHTML = content;
+  return div;
+};
+const handleOutgoingMessage = (userMessage) => {
+  const messsageContent = `<div class="message-text">${userMessage}</div>`;
+  createMessageElement(messsageContent, "user-message");
+};
+//Handle Enter key press for sending messages
 messageInput.addEventListener("keydown", (e) => {
   const userMessage = e.target.value.trim();
   if (e.key === "Enter" && userMessage) {
